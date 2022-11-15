9cfeb760e678ebafa58befae76f5b6542d150eb6
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-15 22:21:07 +0000

    added file structure and boilerplate

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "../src/components/Header"
+import Footer from './components/Footer';
+import PortfolioContainer from './components/PortfolioContainer';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PortfolioContainer />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+
+
+function PortfolioContainer() {
+    return (
+        <h1>Portfolio Container</h1>
+    )
+}
+
+export default PortfolioContainer; 
