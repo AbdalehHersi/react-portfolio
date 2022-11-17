0b630d0eabf1ba7206e813570b086f330dc3376b
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-17 08:06:01 +0000

    added navbar buttons with styling and layout spacing

--- portfolio/src/components/NavbarTabs.js
+++ portfolio/src/components/NavbarTabs.js
@@ -1,25 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faAddressCard, faDiagramProject, faFile, faMessage } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function NavbarTabs({ handlePageChange }) {
+
+    const [currentvisibility, setVisibility] = useState(true);
+
+    const handleToggle = () => {
+        (currentvisibility) ? setVisibility(false) : setVisibility(true)
+        console.log(currentvisibility)
+    }
+
     return (
         <ul className="navbarLinks">
-            <li>
-                <a href="#About" onClick={ () => handlePageChange("About")}>About</a>
-            </li>
 
-            <li>
-                <a href="#Projects" onClick={ () => handlePageChange("Projects")}>Projects</a>
-            </li>
+            <button className="navbtn" onClick={handleToggle}><FontAwesomeIcon icon={faBars} /></button>
 
-            <li>
-                <a href="#Resume" onClick={ () => handlePageChange("Resume")}>Resume</a>
-            </li>
+            <li className="navbarList">
 
-            <li>
-                <a href="#Contact" onClick={ () => handlePageChange("Contact")}>Contact</a>
+                <a
+                    href="#About"
+                    style={(currentvisibility) ? { visibility: "visible" } : { visibility: "hidden" }}
+                    className="navAnchors"
+                    id="about"
+                    onClick={() => handlePageChange("About")}>
+                    <FontAwesomeIcon icon={faAddressCard} />
+                </a>
+
+                <a
+                    href="#Projects"
+                    style={(currentvisibility) ? { visibility: "visible" } : { visibility: "hidden" }}
+                    className="navAnchors"
+                    id="projects"
+                    onClick={() => handlePageChange("Projects")}>
+                    <FontAwesomeIcon icon={faDiagramProject} />
+                </a>
+
+                <a
+                    href="#Resume"
+                    style={(currentvisibility) ? { visibility: "visible" } : { visibility: "hidden" }}
+                    className="navAnchors"
+                    id="resume"
+                    onClick={() => handlePageChange("Resume")}>
+                    <FontAwesomeIcon icon={faFile} />
+                </a>
+
+                <a
+                    href="#Contact"
+                    style={(currentvisibility) ? { visibility: "visible" } : { visibility: "hidden" }}
+                    className="navAnchors"
+                    id="contact"
+                    onClick={() => handlePageChange("Contact")}>
+                    <FontAwesomeIcon icon={faMessage} />
+                </a>
             </li>
-            
         </ul>
     )
 }
