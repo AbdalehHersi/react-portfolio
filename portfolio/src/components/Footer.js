61e490df55cfdf235c8df0252a9045f74dbce4b1
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-21 17:03:20 +0000

    added footer with relevant information

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,8 +1,23 @@
 import React from "react";
+import "./css/Footer.css"
+import icon from "./img/githubicon.png";
+import StackOverflow from "./img/StackOverflow.png";
+import linkedIn from "./img/linkedIn.png";
 
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <div className="footerContainer">
+            <h1>Find me on:</h1>
+            <a href="https://github.com/AbdalehHersi">
+                <img id="githubIcon" src={icon} alt="icon" />
+            </a>
+            <a href="https://stackoverflow.com/users/19364901/abdaleh-hersi">
+                <img id="stackOverflowIcon" src={StackOverflow} alt="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/abdaleh-hersi-605583242/">
+                <img id="linkedInIcon" src={linkedIn} alt="icon" />
+            </a>
+        </div>
     )
 }
 
