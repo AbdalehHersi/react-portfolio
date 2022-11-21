352d70a8f1ee5f7b201810d7078b77e80cf32b14
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-21 22:58:29 +0000

    added contact form with error handling

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,8 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Contact.css";
 
 function Contact() {
+
+    const [element, setElement] = useState("");
+
+    const regexMatch = (email) => {
+        let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        return regex.test(email)
+    }
+
+    const validateForm = () => {
+
+        const inputField = document.getElementById(`${element}`);
+        const errorM = document.getElementById("errorMessage");
+
+        switch (element) {
+            case "name":
+                if (inputField.value) {
+                    errorM.classList.remove("ErrorShown");
+                    errorM.classList.add("ErrorHidden")
+                    errorM.textContent = "";
+                } else {
+                    errorM.classList.remove("ErrorHidden");
+                    errorM.textContent = "You need to enter a name";
+                    errorM.classList.add("ErrorShown");
+                }
+                break;
+
+            case "email":
+                if (regexMatch(inputField.value)) {
+                    errorM.classList.remove("ErrorShown");
+                    errorM.classList.add("ErrorHidden")
+                    errorM.textContent = "";
+                }else {
+                    errorM.classList.remove("ErrorHidden");
+                    errorM.textContent = "You need to enter a valid email";
+                    errorM.classList.add("ErrorShown");
+                }
+                break;
+
+            default: console.log("Default")
+                break;
+        }
+    }
+
     return (
-        <button>Contact</button>
+        <div className="ContactContainer">
+            <form action="" method="get" className="formClass" id="formID">
+                <div className="formFields">
+                    <label htmlFor="name">Enter your name: </label>
+                    <input type="text" name="name" id="name" required onBlur={() => {
+                        setElement("name");
+                        validateForm();
+                    }} />
+                </div>
+                <div className="formFields">
+                    <label htmlFor="email">Enter your email: </label>
+                    <input type="email" name="email" id="email" required onBlur={() => {
+                        setElement("email");
+                        validateForm();
+                    }} />
+                </div>
+                <div className="formFields">
+                    <label htmlFor="message">Enter your message: </label>
+                    <textarea type="message" name="message" id="message" required form="formID" />
+                </div>
+                <div className="formFields">
+                    <button type="submit" id="formBtn">Send</button>
+                </div>
+            </form>
+            <h6 id="errorMessage">Error message</h6>
+        </div>
     )
 }
 
