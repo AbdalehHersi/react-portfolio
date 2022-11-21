dd90fa299d7ca290f7082cb6c90e080b839871ae
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-21 23:42:35 +0000

    added resume section

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -1,8 +1,30 @@
 import React from "react";
+import CV from "../misc/WPCV.pdf"
+import "../css/Resume.css"
 
 function Resume() {
     return (
-        <button>Resume</button>
+        <div id="resumeBody">
+            <a href={CV} alt="Cv" download="CV">Download My CV (Work in Progress)</a>
+            <ul>
+                <h1>Skills</h1>
+                <li>HTML, CSS, Javascript</li>
+                <li>JQuery</li>
+                <li>Bootstrap</li>
+                <li>Moment.js</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>OOP</li>
+                <li>SQL</li>
+                <li>Object Relational Mapping (ORM)</li>
+                <li>MVC paradigm</li>
+                <li>MongoDB</li>
+                <li>PWA</li>
+                <li>React</li>
+                <li>MongoDB</li>
+                <li>IndexedDB</li>
+            </ul>
+        </div>
     )
 }
 
